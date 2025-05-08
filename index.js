34af453f69f130714195d1a237bfae79141f3e4b
DiSantoz <dferreira0191@gmail.com>
2025-05-08 18:03:07 -0400

    add localstorage to save leads:

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// retrieve leads data
+let savedLeads = JSON.parse(localStorage.getItem("myLeads"));
+console.log(savedLeads);
+
+if (savedLeads) {
+  myLeads = savedLeads;
+  renderLeads();
+}
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   console.log(myLeads);
+  // localstorage save leads
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
   inputEl.value = "";
 });
@@ -14,6 +25,7 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   // reset list to blank
   let listItems = "";
+
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `<li>
     <a target='_blank' href='${myLeads[i]}'>  

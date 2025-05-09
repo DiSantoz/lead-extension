050389a26c0df67e6bbcc3ced13514158314ba8c
DiSantoz <dferreira0191@gmail.com>
2025-05-09 11:27:14 -0400

    chrome api to save tabs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
 // retrieve leads data
 const savedLeads = JSON.parse(localStorage.getItem("myLeads"));
 console.log(savedLeads);
@@ -11,6 +13,19 @@ if (savedLeads) {
   myLeads = savedLeads;
   render(myLeads);
 }
+
+// save tab function
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log(tabs);
+
+    console.log(tabs[0].url);
+    let grabbedUrl = tabs[0].url;
+    myLeads.push(grabbedUrl);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 // render leads in list format
 function render(leads) {
